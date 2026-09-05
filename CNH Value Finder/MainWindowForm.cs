6f56e1e333d7716d31a4809b868c0108fa2682c8
Leah Decker <leahdecker1998@gmail.com>
2026-09-05 00:24:01 -0600

    Added very basic initial layout (usage text, run button, tabs). Started adding small features on first tab page

--- CNH Value Finder/MainWindowForm.cs
+++ CNH Value Finder/MainWindowForm.cs
@@ -4,21 +4,103 @@ public partial class MainWindowForm : Form
 {
     public MainWindowForm()
     {
-        Label testLabel = new Label()
+        // Text at top that explains usage
+        Label usageText = new Label()
         {
-            Text = "TEMP",
-            Location = new Point(0, 0),
-            TabIndex = 10
+            Text = "Welcome to the CNH Value Finder! This app lets you find a variety of useful info for CNH GM game design. Select one of the tabs below to get started.",
+            Dock = DockStyle.Top,
+            Size = this.PreferredSize,
         };
 
-        TextBox testTextBox = new TextBox()
+        // Run button at the bottom
+        Button runButton = new Button()
         {
-            Location = new Point(testLabel.Location.X, testLabel.Bounds.Bottom + Padding.Top),
-            TabIndex = 11
+            Text = "Run",
+            Dock = DockStyle.Bottom,
+            Size = this.PreferredSize,
         };
 
-        Controls.Add(testLabel);
-        Controls.Add(testTextBox);
+        // Tabs for different features
+        TabControl tabs = new TabControl()
+        {
+            Dock = DockStyle.Fill,
+            Size = this.PreferredSize,
+        };
+
+        // The different tab pages
+        TabPage diceRollerTabPage = new TabPage()
+        {
+            Text = "Dice Roller",
+            Size = this.PreferredSize,
+            TabIndex = 0,
+        };
+
+        TabPage probabilityTablesTabPage = new TabPage()
+        {
+            Text = "Probability Tables",
+            Size = this.PreferredSize,
+            TabIndex = 1,
+        };
+
+        TabPage statFinderTabPage = new TabPage()
+        {
+            Text = "Stat Finder",
+            Size = this.PreferredSize,
+            TabIndex = 2,
+        };
+
+        // Features for the Dice Roller tab page
+        // Next X are for fillable text options (type dice, number dice, what is a success)
+        // HERE
+        // Next 4 are for Advantage State
+        GroupBox diceRollerAdvantageStateGroupBox = new GroupBox()
+        {
+            Text = "Advantage State",
+            Dock = DockStyle.Fill,
+            Size = this.PreferredSize,
+        };
+
+        RadioButton diceRollerAdvantageRadioButton = new RadioButton()
+        {
+            Text = "Advantage",
+            Dock = DockStyle.Top,
+            Size = this.PreferredSize,
+        };
+
+        RadioButton diceRollerNeutralRadioButton = new RadioButton()
+        {
+            Text = "Neutral",
+            Dock = DockStyle.Top,
+            Size = this.PreferredSize,
+        };
+
+        RadioButton diceRollerDisadvantageRadioButton = new RadioButton()
+        {
+            Text = "Disadvantage",
+            Dock = DockStyle.Top,
+            Size = this.PreferredSize,
+        };
+
+        // Features for the Probability Tables tab page
+
+        // Features for the Stat Finder tab page
+
+        // Add all of the controls and then initialize
+        diceRollerAdvantageStateGroupBox.Controls.Add(diceRollerDisadvantageRadioButton);
+        diceRollerAdvantageStateGroupBox.Controls.Add(diceRollerNeutralRadioButton);
+        diceRollerAdvantageStateGroupBox.Controls.Add(diceRollerAdvantageRadioButton);
+        diceRollerNeutralRadioButton.Checked = true;
+
+        diceRollerTabPage.Controls.Add(diceRollerAdvantageStateGroupBox);
+
+        tabs.Controls.Add(diceRollerTabPage);
+        tabs.Controls.Add(probabilityTablesTabPage);
+        tabs.Controls.Add(statFinderTabPage);
+
+        Controls.Add(tabs);
+        Controls.Add(usageText);
+        Controls.Add(runButton);
+        this.AcceptButton = runButton;
 
         InitializeComponent();
     }
--- CNH Value Finder/obj/Debug/net10.0-windows/CNH.AssemblyInfo.cs
+++ CNH Value Finder/obj/Debug/net10.0-windows/CNH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81de63954b0a9427bdb121db6f0047ebeb074b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c59a9a6e447806a30b4dbfa68cc05b6cefca1a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
